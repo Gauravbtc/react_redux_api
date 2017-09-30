dcb34d4643a92897a3421fe3ad52ca39e72e34cb
Gauravbtc <gauravmakwana93@gmail.com>
2017-09-30 18:25:44 +0530

    signup mailer changes

--- app/controllers/api/v1/confirmations_controller.rb
+++ app/controllers/api/v1/confirmations_controller.rb
@@ -1,12 +1,16 @@
 module Api::V1
-  class ConfirmationsController < Devise::ConfirmationsController 
-  	respond_to :json
+  class ConfirmationsController < Devise::ConfirmationsController
+  	#respond_to :json
   	def show
   		resource = resource_class.confirm_by_token(params[:confirmation_token])
   		if resource.errors.empty?
-	      render :json=> {:success=>true, :message=>"Your account has been confirmed",:login_user => resource}
+        binding.pry
+        resource.auth_token = resource.generate_auth_token
+  		  resource.save
+	      render json: {success: true, message: "Your account has been confirmed",login_user: resource,auth_token: resource.auth_token},status: 200
+        #binding.pry
    	 	else
-   	 		render :json => {:errors =>resource.errors, status: :unprocessable_entity }
+   	 		render json: {errors: resource.errors, status: :unprocessable_entity },status: 203
    		 end
   	end
   end
--- app/models/m_user.rb
+++ app/models/m_user.rb
@@ -2,10 +2,13 @@ class MUser < ApplicationRecord
   has_many :users, dependent: :destroy
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable,:confirmable
- 
+
    def generate_auth_token
    	 token = SecureRandom.hex(10)
    	 return token
    end
 
+   def self.confirmaition_link(token)
+      return Rails.application.secrets.react_host_url + "/users/confirmation/token=#{token}".html_safe
+   end
 end
