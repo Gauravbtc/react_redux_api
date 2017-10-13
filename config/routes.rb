861844b697b735eb3631bda9d61b8757a5f48911
gaurav <gauravm@thinkbiz.co.in>
2017-10-13 18:13:22 +0530

    role fixes

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,7 @@ Rails.application.routes.draw do
   scope module: 'api' do
     namespace :v1 do
       get 'login_user' ,to: 'api#login_user'
+      get 'get_role', to: 'api#get_role'
       resources :users
       devise_for :m_users , :controllers => { sessions: 'api/v1/sessions',
                                              registrations: 'api/v1/registrations',
